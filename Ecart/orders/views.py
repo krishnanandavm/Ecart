09f7e4bb7acf95e05dff21b0716ef7795094f6cc
krishnananda <krishnanandamanoranjan380@gmail.com>
2024-07-25 03:06:28 -0700

    order management- cart to product submission

--- Ecart/orders/views.py
+++ Ecart/orders/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . models import Order, OrderedItem
+from  django.contrib import messages
 from products.models import Product
 
 
@@ -11,8 +12,39 @@ def show_cart(request):
         owner=customer,
         order_status=Order.CART_STAGE
     )
-    ordered_items = OrderedItem.objects.filter(owner=cart_obj)
-    return render(request, 'cart.html', {'cart': cart_obj, 'ordered_items': ordered_items})
+    context = {'cart':cart_obj}
+    return render(request, 'cart.html', context)
+
+def remove_item_from_cart(request, pk):
+    item = OrderedItem.objects.get(pk=pk)
+    if item:
+        item.delete()
+    return redirect('cart')
+
+def checkout_cart(request):
+    if request.POST:
+        try:
+            user = request.user
+            customer = user.customer_profile
+            total = float( request.POST.get('total'))
+            order_obj = Order.objects.get(
+            owner = customer,
+            order_status = Order.CART_STAGE
+            )
+            if order_obj:
+                order_obj.order_status = Order.ORDER_CONFIRMED
+                order_obj.total_price = total
+                order_obj.save()
+                status_message = "your order is processed. Your item will be delivered soon"
+                messages.success(request,status_message)
+            else:
+                status_message = "Unable to processed.No items in cart"
+                messages.error(request,status_message)
+        except Exception as e:
+                status_message = "Unable to processed.No items in cart"
+                messages.error(request,status_message)
+    return redirect('cart')
+
 
 
 def add_to_cart(request):
@@ -29,7 +61,7 @@ def add_to_cart(request):
         ordered_item, created = OrderedItem.objects.get_or_create(
             product = product,
             owner = cart_obj,
-            quantity = quantity
+            quantity = int(quantity)
         )
         if created:
             ordered_item.quantity = quantity
